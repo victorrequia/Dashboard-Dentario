350c55438cfd547ffbe3da95f94ec65700c10bf2
Victor Eduardo Requia <victorrequia@gmail.com>
2024-07-01 18:31:12 -0300

    fix | remocao de arquivos inuteis

--- Dashboard/src/pages/reports.jsx
+++ Dashboard/src/pages/reports.jsx
@@ -1,17 +1,17 @@
 import { Helmet } from 'react-helmet-async';
 
-import { ProductsView } from 'src/sections/products/view';
+import { AppView } from 'src/sections/reports/view';
 
 // ----------------------------------------------------------------------
 
-export default function ProductsPage() {
+export default function AppPage() {
   return (
     <>
       <Helmet>
-        <title> Products | Minimal UI </title>
+        <title> Trabalho Final </title>
       </Helmet>
 
-      <ProductsView />
+      <AppView />
     </>
   );
 }
--- Dashboard/src/routes/sections.jsx
+++ Dashboard/src/routes/sections.jsx
@@ -3,14 +3,14 @@ import { Outlet, Navigate, useRoutes } from 'react-router-dom';
 
 import DashboardLayout from 'src/layouts/dashboard';
 
-export const IndexPage = lazy(() => import('src/pages/forecast'));
-export const EnvironmentPage = lazy(() => import('src/pages/environment'));
-export const ServerPage = lazy(() => import('src/pages/server'));
-export const InverterPage = lazy(() => import('src/pages/inverter'))
+export const IndexPage = lazy(() => import('src/pages/reports'));
+// export const EnvironmentPage = lazy(() => import('src/pages/environment'));
+// export const ServerPage = lazy(() => import('src/pages/server'));
+// export const InverterPage = lazy(() => import('src/pages/inverter'))
 export const BlogPage = lazy(() => import('src/pages/blog'));
 export const UserPage = lazy(() => import('src/pages/user'));
 export const LoginPage = lazy(() => import('src/pages/login'));
-export const ProductsPage = lazy(() => import('src/pages/products'));
+// export const ProductsPage = lazy(() => import('src/pages/products'));
 export const Page404 = lazy(() => import('src/pages/page-not-found'));
 
 // ----------------------------------------------------------------------
@@ -27,12 +27,12 @@ export default function Router() {
       ),
       children: [
         { element: <IndexPage />, index: true },
-        { path: 'environment', element: <EnvironmentPage /> },
-        { path: 'server', element: <ServerPage /> },
-        { path: 'inverter', element: <InverterPage />},
+        // { path: 'environment', element: <EnvironmentPage /> },
+        // { path: 'server', element: <ServerPage /> },
+        // { path: 'inverter', element: <InverterPage />},
         { path: 'user', element: <UserPage /> },
-        { path: 'products', element: <ProductsPage /> },
-        { path: 'blog', element: <BlogPage /> },
+        // { path: 'products', element: <ProductsPage /> },
+        // { path: 'blog', element: <BlogPage /> },
       ],
     },
     {
